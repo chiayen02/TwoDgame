81b9286de061241bf08b9db6b177a1483f8cdc36
Student <vivian910222@gmail.com>
2022-04-26 11:53:02 +0800

    第三次commit

---
+++ app/src/main/java/tw/edu/pu/s1091756/twodgame/MySurfaceView.kt
@@ -0,0 +1,44 @@
+package tw.edu.pu.s1091756.twodgame
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Rect
+import android.util.AttributeSet
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+
+class MySurfaceView(context: Context?, attrs: AttributeSet?) : SurfaceView(context, attrs),
+    SurfaceHolder.Callback{
+    lateinit var surfaceHolder: SurfaceHolder
+    lateinit var BG:Bitmap
+    lateinit var SuperMan: Bitmap
+    init {
+        surfaceHolder = getHolder()
+        BG = BitmapFactory.decodeResource(getResources(), R.drawable.back)
+        SuperMan = BitmapFactory.decodeResource(getResources(), R.drawable.superman)
+        surfaceHolder.addCallback(this)}
+
+    override fun surfaceCreated(holder: SurfaceHolder) {
+        var canvas: Canvas = surfaceHolder.lockCanvas()
+        drawSomething(canvas)
+        surfaceHolder.unlockCanvasAndPost(canvas)
+    }
+    fun drawSomething(canvas:Canvas) {
+        canvas.drawBitmap(BG, 0f, 0f, null)
+        var SrcRect: Rect = Rect(0, 0, SuperMan.width, SuperMan.height) //裁切
+        var w:Int = SuperMan.width / 6
+        var h:Int = SuperMan.height / 6
+        var DestRect:Rect = Rect(0, 0, w, h)
+        canvas.drawBitmap(SuperMan, SrcRect, DestRect, null)
+    }
+
+    override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
+
+    }
+
+    override fun surfaceDestroyed(holder: SurfaceHolder) {
+
+    }
+}
